f825dbb0ba7656ce09f979fef2ffd4bf5af8a74d
Manu343726 <Manu343726@gmail.com>
2015-04-08 21:28:29 +0200

    Remove sources always

--- generate.sh
+++ generate.sh
@@ -5,12 +5,12 @@ sources=("${entry_point}" "utils.py")
 remote="https://raw.githubusercontent.com/Manu343726/biiblock_generator/master"
 
 for source in "${sources[@]}"; do
-    wget -O "${source}" "${remote}/${source}"
+    wget -q -N -O "${source}" "${remote}/${source}"
 done
 
-''''which python2 >/dev/null 2>&1 && python2 "./${entry_point}" "$@" # '''
-''''which python  >/dev/null 2>&1 && python  "./${entry_point}" "$@" # '''
-''''echo "Error: I can't find python anywhere"                       # '''
+''''which python2 >/dev/null 2>&1 && exec python2 "./${entry_point}" "$@" # '''
+''''which python  >/dev/null 2>&1 && exec python  "./${entry_point}" "$@" # '''
+''''exec echo "Error: I can't find python anywhere"                       # '''
 
 for source in "${sources[@]}"; do
     rm -fv "./${source}"
